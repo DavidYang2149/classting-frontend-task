1f52ab8d99fabcbced43523dc048d4bbe7b7b953
Yang Hong Seok <davidyang2149@gmail.com>
2022-09-19 17:31:00 +0900

    :hammer: 각 컴포넌트에 커스텀훅을 연결합니다
    - 메인, 질문, 결과 컴포넌트

--- src/components/question/Question.tsx
+++ src/components/question/Question.tsx
@@ -1,36 +1,131 @@
 import React from 'react';
 
+import useQuestion from 'src/hooks/question/useQuestion';
+
 const Question = () => {
+  const {
+    currentIndex, examPaper, answerPaper,
+    handleClickAnswer,
+    handleClickPrevMoveQuestion,
+    handleClickNextMoveQuestion,
+    handleClickShowReport,
+  } = useQuestion();
+
+  const quiz = examPaper[currentIndex];
+
+  if (!quiz) {
+    return (<div>Loading</div>);
+  }
+
+  const isMarkingAnswer = answerPaper.length >= currentIndex + 1;
+
+  const isMarkingCorrect = isMarkingAnswer && answerPaper[currentIndex].isCorrect;
+  const answerNumber = isMarkingAnswer && quiz.answers.findIndex((question) => question.answer === answerPaper[currentIndex].answer) + 1;
+  const correctNumber = isMarkingAnswer && quiz.answers.findIndex((question) => question.isCorrect === true) + 1;
+
+  const isPrevQuiz = currentIndex > 0;
+  const isNextQuiz = answerPaper[currentIndex] && currentIndex !== examPaper.length - 1;
+  const isEndQuiz = currentIndex === examPaper.length - 1 && answerPaper.length === examPaper.length;
+
   return (
     <>
       <div>
-        <h4>문제 4</h4>
-        <h2>다음주 3대장 프론트엔드 프레임워크가 아닌 것은?</h2>
+        <h4>
+          문제
+          {' '}
+          {currentIndex + 1}
+        </h4>
+        <h2>
+          {quiz.question}
+        </h2>
       </div>
       <div>
         <li className="list-none">
-          <ul>1. 앵귤러</ul>
-          <ul>2. 리액트</ul>
-          <ul>3. 뷰</ul>
-          <ul>4. HTML</ul>
+          {quiz.answers.map(({ answer }, index) => {
+            return (
+              <ul key={answer}>
+                {index + 1}
+                .
+                {' '}
+                {answer}
+              </ul>
+            );
+          })}
         </li>
       </div>
       <div>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button">4</button>
+        {quiz.answers.map(({ answer }, index) => {
+          return (
+            <button
+              key={answer}
+              type="button"
+              value={answer}
+              onClick={handleClickAnswer}
+              disabled={isMarkingAnswer}
+            >
+              {index + 1}
+            </button>
+          );
+        })}
       </div>
+      {
+        isMarkingAnswer && (
+          <div>
+            <h3>
+              {isMarkingCorrect ? '정답' : '오답'}
+              입니다.
+            </h3>
+            <p>
+              선택한 답
+              {' '}
+              {answerNumber}
+              번
+            </p>
+            <p>
+              정답
+              {' '}
+              {correctNumber}
+              번
+            </p>
+          </div>
+        )
+      }
       <div>
-        <h3>정답입니다.</h3>
-        <h3>오답입니다.</h3>
-        <p>선택한 답 2번</p>
-        <p>정답 4번</p>
-      </div>
-      <div>
-        <button type="button">이전문제</button>
-        <p>3 / 5</p>
-        <button type="button">다음문제</button>
+        {
+          isPrevQuiz && (
+            <button
+              type="button"
+              onClick={handleClickPrevMoveQuestion}
+            >
+              이전문제
+            </button>
+          )
+        }
+        <p>
+          {currentIndex + 1}
+          {' / '}
+          {examPaper.length}
+        </p>
+        {
+          isNextQuiz && (
+            <button
+              type="button"
+              onClick={handleClickNextMoveQuestion}
+            >
+              다음문제
+            </button>
+          )
+        }
+        {
+          isEndQuiz && (
+            <button
+              type="button"
+              onClick={handleClickShowReport}
+            >
+              결과 보기
+            </button>
+          )
+        }
       </div>
     </>
   );
--- src/components/report/Report.tsx
+++ src/components/report/Report.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
+import useReport from 'src/hooks/report/useReport';
+
 const Report = () => {
+  const {
+    correctCount, inCorrectCount, recordTime,
+    isDone,
+    handleClickReExam,
+    handleClickReturnMain,
+  } = useReport();
+
+  if (!isDone) {
+    return (<div>Loading</div>);
+  }
+
   return (
     <>
       <div>
@@ -9,23 +22,28 @@ const Report = () => {
       <div>
         <li className="list-none">
           <ul>
-            <p>평균난이도</p>
-            <p>3.5 / 5</p>
-          </ul>
-          <ul>
             <p>정답수 / 오답수 / 문항수</p>
-            <p>2 / 5</p>
+            <p>
+              {correctCount}
+              {' '}
+              /
+              {' '}
+              {inCorrectCount}
+              {' '}
+              /
+              {' '}
+              {correctCount + inCorrectCount}
+            </p>
           </ul>
           <ul>
             <p>내풀이시간</p>
-            <p>10분 20초</p>
+            <p>{recordTime}</p>
           </ul>
         </li>
       </div>
       <div>
-        <button type="button">다시 풀기</button>
-        <button type="button">오답 노트</button>
-        <button type="button">닫기</button>
+        <button type="button" onClick={handleClickReExam}>다시 풀기</button>
+        <button type="button" onClick={handleClickReturnMain}>닫기</button>
       </div>
     </>
   );
