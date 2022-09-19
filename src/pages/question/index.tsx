db8a8312838c4c5bc7a7123f5020c3dfff26c9d1
Yang Hong Seok <davidyang2149@gmail.com>
2022-09-19 17:31:39 +0900

    :hammer: 잘못된 URL 접속을 막기 위해 방어 코드를 심습니다
    - 질문, 결과 페이지

--- src/pages/question/index.tsx
+++ src/pages/question/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
 
 import AppBar from 'src/components/common/AppBar';
 import CustomHead from 'src/components/common/CustomHead';
 import Question from 'src/components/question/Question';
+import useQuestion from 'src/hooks/question/useQuestion';
 
 const QuestionPage = () => {
+  const router = useRouter();
+  const { examPaper } = useQuestion();
+
+  useEffect(() => {
+    if (examPaper.length < 1) {
+      router.replace('/');
+    }
+  }, [router, examPaper]);
+
   return (
     <div>
       <CustomHead
--- src/pages/report/index.tsx
+++ src/pages/report/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
 
 import AppBar from 'src/components/common/AppBar';
 import CustomHead from 'src/components/common/CustomHead';
 import Report from 'src/components/report/Report';
+import useReport from 'src/hooks/report/useReport';
 
 const QuestionPage = () => {
+  const router = useRouter();
+  const { isDone } = useReport();
+
+  useEffect(() => {
+    if (!isDone) {
+      router.replace('/');
+    }
+  }, [router, isDone]);
+
   return (
     <div>
       <CustomHead
