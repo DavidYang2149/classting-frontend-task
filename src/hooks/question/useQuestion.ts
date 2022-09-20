005cd56ce131ca0d4be8e7226497f4f665f17ab0
Yang Hong Seok <davidyang2149@gmail.com>
2022-09-21 00:13:22 +0900

    :hammer: 질문 페이지의 번호를 쿼리스트링으로 받게 만듭니다
    - 뒤로가기 버튼 사용 가능

--- src/hooks/question/useQuestion.ts
+++ src/hooks/question/useQuestion.ts
@@ -1,30 +1,30 @@
 import { useRouter } from 'next/router';
 
 import { useAppDispatch, useRootState } from 'src/redux/store';
-import { moveCurrentIndex, selectAnswer, showReport } from 'src/redux/user/examinee';
+import { selectAnswer, showReport } from 'src/redux/user/examinee';
 
 const useQuestion = () => {
   const router = useRouter();
   const dispatch = useAppDispatch();
 
+  const { index } = router.query;
+
+  const currentIndex = Number.parseInt(index as string, 10) || 0;
+
   const { examPaper } = useRootState((state) => (state.exam));
-  const { currentIndex, answerPaper } = useRootState((state) => (state.user));
+  const { answerPaper } = useRootState((state) => (state.user));
 
   const handleClickAnswer = (event: React.MouseEvent<HTMLButtonElement>) => {
     const { value } = (event.target as HTMLButtonElement);
-    dispatch(selectAnswer(value));
-  };
-
-  const handleClickMoveQuestion = (value: number) => {
-    dispatch(moveCurrentIndex(value));
+    dispatch(selectAnswer(value)(currentIndex));
   };
 
   const handleClickPrevMoveQuestion = () => {
-    handleClickMoveQuestion(currentIndex - 1);
+    router.push(`question?index=${currentIndex - 1}`);
   };
 
   const handleClickNextMoveQuestion = () => {
-    handleClickMoveQuestion(currentIndex + 1);
+    router.push(`question?index=${currentIndex + 1}`);
   };
 
   const handleClickShowReport = () => {
