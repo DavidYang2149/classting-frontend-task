a257d110029d991403752b95defb66c9a8ba6ad7
Yang Hong Seok <davidyang2149@gmail.com>
2022-09-20 18:20:07 +0900

    :hammer: 404 페이지를 구축합니다

---
+++ src/components/common/NotFound.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="not-found-content" role="status">
+      <div className="flex flex-col items-center">
+        <Image
+          src="/404.svg"
+          alt="not-found"
+          width={600}
+          height={600}
+        />
+        <Link href="/">
+          <a className="not-found-button">메인으로 돌아가기</a>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+import NotFound from 'src/components/common/NotFound';
+
+const Custom404 = () => {
+  return (
+    <NotFound />
+  );
+};
+
+export default Custom404;
