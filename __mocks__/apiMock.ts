f55f7402db67cbe012639fc5e88e18224a3ee459
Yang Hong Seok <davidyang2149@gmail.com>
2022-09-21 22:44:06 +0900

    :clown_face: Mock 데이터를 추가합니다

---
+++ __mocks__/apiMock.ts
@@ -0,0 +1,69 @@
+/* eslint-disable max-len */
+export const mockApiQuestions = {
+  response_code: 0,
+  results: [
+    {
+      category: 'General Knowledge',
+      type: 'multiple',
+      difficulty: 'medium',
+      question: 'This field is sometimes known as “The Dismal Science.”',
+      correct_answer: 'Economics',
+      incorrect_answers: [
+        'Physics',
+        'Politics',
+        'Philosophy',
+      ],
+    },
+    {
+      category: 'Mythology',
+      type: 'multiple',
+      difficulty: 'hard',
+      question: 'Nidhogg is a mythical creature from what mythology?',
+      correct_answer: 'Norse',
+      incorrect_answers: [
+        'Hindu',
+        'Greek',
+        'Egyptian',
+      ],
+    },
+    {
+      category: 'Geography',
+      type: 'multiple',
+      difficulty: 'easy',
+      question: 'Which country is the home of the largest Japanese population outside of Japan?',
+      correct_answer: 'Brazil',
+      incorrect_answers: [
+        'Russia',
+        'China',
+        'The United States',
+      ],
+    },
+    {
+      category: 'Entertainment: Television',
+      type: 'multiple',
+      difficulty: 'medium',
+      question: 'In the television show "Lazy Town", who is the actor of Robbie Rotten?',
+      correct_answer: 'Stefán Stefánsson',
+      incorrect_answers: [
+        'Magnús Scheving',
+        'Adam Sandler',
+        'Stephen Carl',
+      ],
+    },
+    {
+      category: 'Entertainment: Japanese Anime & Manga',
+      type: 'multiple',
+      difficulty: 'easy',
+      question: 'In the Naruto manga, what is the last name of Tsunade?',
+      correct_answer: 'Senju',
+      incorrect_answers: [
+        'Yamanaka',
+        'Haruno',
+        'Uzumaki',
+      ],
+    },
+  ],
+};
+
+// TODO: Mock 데이터가 2개 이상일 때 삭제할 것
+export const XXX = '';
