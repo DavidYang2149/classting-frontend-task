6f7c466adf4527b4cfeb0b0e50c3b77cecf2cd7a
Yang Hong Seok <davidyang2149@gmail.com>
2022-09-21 11:11:37 +0900

    :hammer: 메인 화면에서 로딩 컴포넌트를 추가합니다

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,17 @@ import type { NextPage } from 'next';
 
 import AppBar from 'src/components/common/AppBar';
 import CustomHead from 'src/components/common/CustomHead';
+import Loading from 'src/components/common/Loading';
 import StartButton from 'src/components/main/StartButton';
+import useMain from 'src/hooks/main/useMain';
 
 const Home: NextPage = () => {
+  const { isLoading } = useMain();
+
+  if (isLoading) {
+    return (<Loading />);
+  }
+
   return (
     <div>
       <CustomHead
