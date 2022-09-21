21b7d5933ea8e20a8716cb7779f244d4dd1a316a
Yang Hong Seok <davidyang2149@gmail.com>
2022-09-21 22:49:02 +0900

    :white_check_mark: 페이지 테스트코드를 작성합니다

--- __tests__/question.test.tsx
+++ __tests__/question.test.tsx
@@ -4,14 +4,17 @@
 
 import React from 'react';
 import { render } from '@testing-library/react';
+import mockRouter from 'next-router-mock';
 
-import mockRootState, { mockUseDispatch, mockUseSelector } from '__mocks__/reduxMock';
-import Home from 'src/pages/index';
+import mockRootState, { mockStartExamState, mockUseDispatch, mockUseSelector } from '__mocks__/reduxMock';
+import QuestionPage from 'src/pages/question';
 import { RootState } from 'src/redux/store';
 
+// eslint-disable-next-line global-require
+jest.mock('next/router', () => require('next-router-mock'));
 jest.mock('react-redux');
 
-describe('메인화면', () => {
+describe('문제화면', () => {
   const dispatch = jest.fn();
 
   beforeEach(() => {
@@ -19,27 +22,26 @@ describe('메인화면', () => {
     dispatch.mockClear();
     mockUseDispatch.mockImplementation(() => dispatch);
     mockUseSelector.mockImplementation((selector: (arg: RootState) => void) => selector({
-      ...mockRootState,
+      ...mockStartExamState,
     }));
   });
 
-  it('메인 타이틀을 확인합니다', () => {
-    const { container } = render(<Home />);
+  it('문제 페이지 화면을 확인합니다', () => {
+    const { container } = render(<QuestionPage />);
 
     expect(container).toHaveTextContent(/Classting Quiz/i);
+    expect(container).toHaveTextContent('문제 1');
+    expect(container).toHaveTextContent('1 / 5');
   });
 
-  context('메인 화면이 로딩 중일 때', () => {
+  context('문제(지문)가 존재하지 않을 경우', () => {
     it('로딩 화면을 보여줍니다', () => {
+      mockRouter.setCurrentUrl('/question');
       mockUseSelector.mockImplementation((selector: (arg: RootState) => void) => selector({
         ...mockRootState,
-        exam: {
-          ...mockRootState.exam,
-          isLoading: true,
-        },
       }));
 
-      const { container } = render(<Home />);
+      const { container } = render(<QuestionPage />);
 
       expect(container).toHaveTextContent('Loading...');
     });
