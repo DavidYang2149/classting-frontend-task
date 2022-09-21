e91f63109bfdaa1ed6226ef9c63a30e5c81ae0db
Yang Hong Seok <davidyang2149@gmail.com>
2022-09-21 12:06:32 +0900

    :hammer: 내비게이션바의 '클래스팅퀴즈'를 클릭하면 메인화면으로 돌아갑니다

--- src/components/common/AppBar.tsx
+++ src/components/common/AppBar.tsx
@@ -1,9 +1,14 @@
+import Link from 'next/link';
 import React from 'react';
 
 const AppBar = () => {
   return (
     <header className="app-bar">
-      <h1 className="app-bar-title">CLASSTING QUIZ</h1>
+      <h1 className="app-bar-title">
+        <Link href="/">
+          CLASSTING QUIZ
+        </Link>
+      </h1>
     </header>
   );
 };
