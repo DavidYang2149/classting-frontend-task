e2f45c70f7b7b2a28669c424bb6d0adf9210205f
Yang Hong Seok <davidyang2149@gmail.com>
2022-09-21 12:28:00 +0900

    :hammer: 메타 데이터를 고도화합니다

--- src/components/common/CustomHead.tsx
+++ src/components/common/CustomHead.tsx
@@ -11,6 +11,19 @@ const CustomHead = ({
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="description" content={content} />
+      <meta name="keywords" content="classting, quiz" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:site_name" content="classting quiz" />
+      <meta property="og:description" content={content} />
+      <meta property="og:image" content="/favicon.png" />
+      <meta property="og:url" content="https://classting-frontend-task.vercel.app" />
+
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={content} />
+      <meta name="twitter:image" content="/favicon.png" />
+
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <link rel="icon" href="/favicon.png" />
